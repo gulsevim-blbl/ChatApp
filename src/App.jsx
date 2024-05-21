3d499391c646c9b5990d6b8670c848aba88f1590
gulsevim-blbl <gulsevimblbl@gmail.com>
2024-05-21 17:11:33 +0300

    backend completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
+import { useEffect } from "react";
 import  Chat  from "./components/chat/Chat"
 import  Detail  from "./components/detail/Detail"
 import List  from "./components/list/List"
 import LoginPage from "./components/login/LoginPage";
 import Notification from "./components/notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore";
 
 
  const App = () => {
-  const user = false;
+  
+  const {currentUser,isLoading,fetchUserInfo} = useUserStore()
+  const {chatId} = useChatStore()
+
+  useEffect(()=>{
+    const unSub = onAuthStateChanged(auth,(user)=>{
+      fetchUserInfo(user?.uid)
+    })
+    
+    return () => {
+      unSub();
+    }
+  },[fetchUserInfo])
+
+
+    if(isLoading) return <div className="loading">Loading...</div>
+
+
 
   return (
     <div className='container'>
           {
-            user ? (
+            currentUser ? (
               <>
                <List/>
-               <Chat/>
-               <Detail/>
+               {chatId && <Chat/>}
+               {chatId && <Detail/>}
               </>
              
             ) :
--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,11 +1,25 @@
 import { useEffect, useRef, useState } from "react"
 import "./chat.css"
 import EmojiPicker from "emoji-picker-react"
+import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore"
+import { db } from "../../lib/firebase"
+import { useChatStore } from "../../lib/chatStore"
+import { useUserStore } from "../../lib/userStore"
+import upload from "../../lib/upload"
 
 
 export const Chat = () => {
+  const [chat,setChat] = useState()
   const [open,setOpen] = useState(false)
   const [text,setText] = useState("")
+  const [img,setImg] = useState({
+    file:null,
+    url:"",
+  });
+
+  const {currentUser} = useUserStore()
+  const {chatId, user, isCurrentUserBlocked, isReceiverBlocked} = useChatStore()
+  
 
   const endRef = useRef(null) /** 79.satırda fonksiyonu tanımladık  */
 
@@ -13,18 +27,92 @@ export const Chat = () => {
     endRef.current?.scrollIntoView({behavior:"smooth"})
   })
 
+  useEffect(()=>{
+    const unsub = onSnapshot(doc(db,"chats",chatId),(res)=>{
+        setChat(res.data())
+    })
+
+    return () =>{
+      unsub()
+    }
+  },[chatId])
+
   const handleEmoji = e => {
     setText((prev)=> prev+ e.emoji);
     setOpen(false);
   }
 
+
+  const handleImg = e => {
+    if(e.target.files[0]){
+
+      setImg({
+         file:e.target.files[0],
+         url:URL.createObjectURL(e.target.files[0])
+      })
+    }  
+  }
+
+  const handleSend = async () => {
+    if (text === "") return;
+
+    let imgUrl = null;
+
+    try {
+      if (img.file) {
+        imgUrl = await upload(img.file);
+      }
+
+      await updateDoc(doc(db, "chats", chatId), {
+        messages: arrayUnion({
+          senderId: currentUser.id,
+          text,
+          createdAt: new Date(),
+          ...(imgUrl && { img: imgUrl }),
+        }),
+      });
+
+      const userIDs = [currentUser.id, user.id];
+
+      userIDs.forEach(async (id) => {
+        const userChatsRef = doc(db, "userchats", id);
+        const userChatsSnapshot = await getDoc(userChatsRef);
+
+        if (userChatsSnapshot.exists()) {
+          const userChatsData = userChatsSnapshot.data();
+
+          const chatIndex = userChatsData.chats.findIndex(
+            (c) => c.chatId === chatId
+          );
+
+          userChatsData.chats[chatIndex].lastMessage = text;
+          userChatsData.chats[chatIndex].isSeen = id === currentUser.id? true : false;
+          userChatsData.chats[chatIndex].updatedAt = Date.now();
+
+          await updateDoc(userChatsRef, {
+            chats: userChatsData.chats,
+          });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    } finally{
+    setImg({
+      file: null,
+      url: "",
+    });
+
+    setText("");
+    }
+  };
+
   return (
     <div className='chat'>
       <div className="top">
         <div className="user">
-          <img src="./avatar.png" alt=""/>
+          <img src={ user?.avatar || "./avatar.png" }alt=""/>
           <div className="texts">
-            <span>Eren</span>
+            <span>{user.username}</span>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
         </div>
@@ -36,65 +124,55 @@ export const Chat = () => {
       </div>
 
       <div className="center">
-        <div className="message ">
-          <img src="./avatar.png" alt="" />
+        {chat?.messages?.map((messages) =>(
+        <div className={messages.senderId === currentUser?.id ? "message own" : "message"} key={messages?.createAt}>
           <div className="texts">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, in.</p>
-            <span>1 min ago</span>
+          {messages.img && <img
+               src={messages.img}
+                alt="" 
+            />}
+            <p>{messages.text}</p>
+            {/* <span>{messages}</span> */}
           </div>
         </div>
-        <div className="message own">
-          <div className="texts">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, in.</p>
-            <span>1 min ago</span>
-          </div>
-        </div>
-        <div className="message ">
-          <img src="./avatar.png" alt="" />
-          <div className="texts">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, in.</p>
-            <span>1 min ago</span>
-          </div>
-        </div>
-        <div className="message own">
-          <div className="texts">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, in.</p>
-            <span>1 min ago</span>
-          </div>
-        </div>
-        <div className="message ">
-        <img src="./avatar.png" alt="" />
-          <div className="texts">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, in.</p>
-            <span>1 min ago</span>
-          </div>
-        </div>
-        <div className="message own">
-          <div className="texts">
-            <img src="https://w0.peakpx.com/wallpaper/494/397/HD-wallpaper-istanbul-kopru-bogaz-istanbul-turkey-turkiye.jpg" alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, in.</p>
-            <span>1 min ago</span>
-          </div>
-        </div>
+          )) }
+
+         { img.url && <div className="message own">
+            <div className="texts">
+              <img src={img.url} alt="" />
+            </div>
+          </div>}
           <div ref={endRef}></div>  
       </div>
 
       <div className="bottom">
        
         <div className="icons">
-          <img src="./img.png" alt="" />
+          <label htmlFor="file">
+             <img src="./img.png" alt="" />
+          </label>
+          <input type="file" id="file" style={{display:"none"}} onChange={handleImg}/>
           <img src="./camera.png" alt="" />
           <img src="./mic.png" alt="" />
         </div>
-
-        <input type="text" placeholder="Type a message..." value={text}  onChange={e=>setText(e.target.value)}/>
+        <input
+         type="text" 
+         placeholder={
+          isCurrentUserBlocked || isReceiverBlocked 
+          ? "You cannot send a message" 
+          : "Type a message..." 
+        }
+         value={text}  
+         onChange={e=>setText(e.target.value)}
+         disabled={isCurrentUserBlocked || isReceiverBlocked}
+        />
         <div className="emoji">
           <img src="./emoji.png" alt="" onClick={() => setOpen((prev) => !prev)}/>
           <div className="picker">
               <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
           </div>
         </div>
-        <button className="sendButton">Send</button>
+        <button className="sendButton" onClick={handleSend} disabled={isCurrentUserBlocked || isReceiverBlocked}>Send</button>
       </div>
     </div>
   )
